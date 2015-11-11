075f1bebb4491cb0f2342e1137cdcc6862fb5816
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2015-11-11 15:04:21 -0600

    Start of Advanced Android App Development: Integration Points and Error Cases.
    Added the OpenWeatherMap.org API key under Constants.java.
    
    Updated SunshineSyncAdapter.java, ForecastFragment.java, fragment_main.xml and strings.xml to handle the error case where no network is available on initial launch.
    
    Minor updates to build.gradle.

---
+++ app/src/main/java/com/example/android/sunshine/app/Constants.java
@@ -0,0 +1,9 @@
+package com.example.android.sunshine.app;
+
+public class Constants {
+    // TODO: Update value before launching app or before pushing code to GitHub
+    /*
+    * To fetch weather, you will use the API from openweathermap.org.
+    */
+    public static final String api_key = "api_key"; // update before launching app
+}
