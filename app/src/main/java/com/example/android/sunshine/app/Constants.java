675efd475148aa2fe4cf509b67a407c7f40ff990
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2016-01-06 13:33:45 -0600

    Clean-up.

--- app/src/main/java/com/example/android/sunshine/app/Constants.java
+++ app/src/main/java/com/example/android/sunshine/app/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     /*
     * To fetch weather, you will use the API from openweathermap.org.
     */
-    public static final String api_key = "a4f1845f334efb945644d7ce2b5bb5a0"; // TODO: Update value before launching app or before pushing code to GitHub
+    public static final String api_key = "api_key"; // TODO: Update value before launching app or before pushing code to GitHub
 }
